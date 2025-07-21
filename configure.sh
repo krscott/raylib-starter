68fc7b965adeaf19066574a827053c011f19f89a
Kris Scott <kscott91@gmail.com>
2025-07-21 02:46:44 -0700

    Add configure --zig option for windows

--- configure.sh
+++ configure.sh
@@ -3,31 +3,76 @@ set -euo pipefail
 shopt -s failglob
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-platform="${1:-desktop}"
-
 usage() {
-    echo "$0 [PLATFORM]"
+    echo "$0 [-h] [-z] [PLATFORM]" >&2
 }
 
-case "$platform" in
+show_help() {
+    usage
+    cat <<EOS >&2
+Options:
+  -h  --help    Show help and exit
+  -z  --zig     Use zig cc
+EOS
+}
+
+platform=
+use_zig=
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --help | -h)
+        show_help
+        exit
+        ;;
+    --zig | -z)
+        use_zig=1
+        ;;
+    -*)
+        echo "Unknown option: $1" >&2
+        usage
+        exit 1
+        ;;
+    *)
+        if [[ -z $platform ]]; then
+            platform="$1"
+        else
+            echo "Too many positional arguments" >&2
+            usage
+        fi
+        ;;
+    esac
+
+    shift 1 # Move to the next argument
+done
+
+case "${platform:-desktop}" in
 desktop)
     cmake -B build/desktop
     ;;
 
 windows | win)
-    # https://jcbhmr.com/2024/07/19/zig-cc-cmake/
-    ASM="zig cc" \
-        CC="zig cc" \
-        CXX="zig c++" \
-        cmake \
-        -DCMAKE_SYSTEM_NAME="Windows" \
-        -DCMAKE_SYSTEM_PROCESSOR="x86_64" \
-        -DCMAKE_ASM_COMPILER_TARGET="x86_64-windows-gnu" \
-        -DCMAKE_C_COMPILER_TARGET="x86_64-windows-gnu" \
-        -DCMAKE_CXX_COMPILER_TARGET="x86_64-windows-gnu" \
-        -DCMAKE_AR="$PWD/zig-ar" \
-        -DCMAKE_RANLIB="$PWD/zig-ranlib" \
-        -B build/windows
+    if [[ $use_zig == 1 ]]; then
+        # https://jcbhmr.com/2024/07/19/zig-cc-cmake/
+        ASM="zig cc" \
+            CC="zig cc" \
+            CXX="zig c++" \
+            cmake -B build/windows \
+            -DCMAKE_SYSTEM_NAME="Windows" \
+            -DCMAKE_SYSTEM_PROCESSOR="x86_64" \
+            -DCMAKE_ASM_COMPILER_TARGET="x86_64-windows-gnu" \
+            -DCMAKE_C_COMPILER_TARGET="x86_64-windows-gnu" \
+            -DCMAKE_CXX_COMPILER_TARGET="x86_64-windows-gnu" \
+            -DCMAKE_AR="$PWD/zig-ar" \
+            -DCMAKE_RANLIB="$PWD/zig-ranlib"
+    else
+        cmake -B build/windows \
+            -DCMAKE_SYSTEM_NAME="Windows" \
+            -DCMAKE_SYSTEM_PROCESSOR="x86_64" \
+            -DCMAKE_ASM_COMPILER_TARGET="x86_64-windows-gnu" \
+            -DCMAKE_C_COMPILER_TARGET="x86_64-windows-gnu" \
+            -DCMAKE_CXX_COMPILER_TARGET="x86_64-windows-gnu"
+    fi
     ;;
 
 web)
--- pkgs/windows.nix
+++ pkgs/windows.nix
@@ -11,13 +11,4 @@ callPackage ./base.nix {
   mainProgram = "${appName}.exe";
 
   inherit appName raylib-src;
-
-  configurePhase = ''
-    cmake -B build/windows \
-      -DCMAKE_SYSTEM_NAME="Windows" \
-      -DCMAKE_SYSTEM_PROCESSOR="x86_64" \
-      -DCMAKE_ASM_COMPILER_TARGET="x86_64-windows-gnu" \
-      -DCMAKE_C_COMPILER_TARGET="x86_64-windows-gnu" \
-      -DCMAKE_CXX_COMPILER_TARGET="x86_64-windows-gnu"
-  '';
 }
